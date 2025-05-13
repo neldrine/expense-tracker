633ddac6fdc6da39af4113875c8bedb8781e516a
Neldrine <neldrine@gmail.com>
2025-05-13 10:50:37 +0100

    refactored expense controller to its own api directory and added jwt trait to user model

--- expense-tracker-backend/app/Http/Controllers/Api/ExpenseController.php
+++ expense-tracker-backend/app/Http/Controllers/Api/ExpenseController.php
@@ -1,9 +1,10 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Api;
 
 use App\Models\Expense;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class ExpenseController extends Controller
 {
